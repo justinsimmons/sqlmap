07dacda15f7a7184ae657c27f55548f8f7c21e7a
Justin Simmons <justin.simmons@airspacelink.com>
2023-09-12 21:18:54 +0100

    Add the remaining unwrap functions

--- protobuf.go
+++ protobuf.go
@@ -1,4 +1,4 @@
-// Copyright 2023 Justin Simmons
+// Copyright 2023 Justin Simmons.
 //
 // This file is part of sqlmap.
 // sqlmap is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation, either version 3 of the License, or any later version.
--- unwrap.go
+++ unwrap.go
@@ -1,4 +1,4 @@
-// Copyright 2023 Justin Simmons
+// Copyright 2023 Justin Simmons.
 //
 // This file is part of sqlmap.
 // sqlmap is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation, either version 3 of the License, or any later version.
@@ -46,4 +46,70 @@ func UnwrapTimestamp(t sql.NullTime) *timestamppb.Timestamp {
 	}
 
 	return timestamppb.New(tm)
+}
+
+// UnwrapInt64 unwraps the sql.NullInt64 to a int64 pointer.
+func UnwrapInt64(in sql.NullInt64) *int64 {
+	var nInt *int64
+
+	if in.Valid {
+		nInt = &in.Int64
+	}
+
+	return nInt
+}
+
+// UnwrapInt32 unwraps the sql.NullInt32 to a int32 pointer.
+func UnwrapInt32(in sql.NullInt32) *int32 {
+	var nInt *int32
+
+	if in.Valid {
+		nInt = &in.Int32
+	}
+
+	return nInt
+}
+
+// UnwrapInt16 unwraps the sql.NullInt16 to a int16 pointer.
+func UnwrapInt16(in sql.NullInt16) *int16 {
+	var nInt *int16
+
+	if in.Valid {
+		nInt = &in.Int16
+	}
+
+	return nInt
+}
+
+// UnwrapByte unwraps the sql.NullByte to a byte pointer.
+func UnwrapByte(in sql.NullByte) *byte {
+	var nByte *byte
+
+	if in.Valid {
+		nByte = &in.Byte
+	}
+
+	return nByte
+}
+
+// UnwrapFloat64 unwraps the sql.NullFloat64 to a float64 pointer.
+func UnwrapFloat64(in sql.NullFloat64) *float64 {
+	var nFloat *float64
+
+	if in.Valid {
+		nFloat = &in.Float64
+	}
+
+	return nFloat
+}
+
+// UnwrapBoolean unwraps the sql.NullBool to a boolean pointer.
+func UnwrapBoolean(in sql.NullBool) *bool {
+	var nBool *bool
+
+	if in.Valid {
+		nBool = &in.Bool
+	}
+
+	return nBool
 }
