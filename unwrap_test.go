4e5f86dfeb5df9cd80beb87bc07f35f85c37c0c3
Justin Simmons <justin.simmons@airspacelink.com>
2024-01-22 15:33:57 -0500

    Update copyright notice to include 2024

--- unwrap_test.go
+++ unwrap_test.go
@@ -1,3 +1,10 @@
+// Copyright 2023, 2024 Justin Simmons.
+//
+// This file is part of sqlmap.
+// sqlmap is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation, either version 3 of the License, or any later version.
+// sqlmap is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+// You should have received a copy of the GNU Lesser General Public License along with sqlmap. If not, see <https://www.gnu.org/licenses/>.
+
 package sqlmap
 
 import (
